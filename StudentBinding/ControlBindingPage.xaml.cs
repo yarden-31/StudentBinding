b1b4f63796f9f3531ec0ab17051f46632b04e9d7
Owner <stam1@gmail.com>
2026-05-10 15:33:48 +0300

    AddedPreviousPage

---
+++ StudentBinding/ControlBindingPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace StudentBinding
+{
+    public partial class ControlBindingPage : ContentPage
+    {
+       
+        public ControlBindingPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OpacityChangeOnClick(object sender, EventArgs e)
+        {
+            if (botImg.Opacity > 0)
+            {
+                botImg.Opacity = 0;
+            }
+            else
+                botImg.Opacity = 1;
+        }
+    }
+
+}
