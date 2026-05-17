47c764b258bb11649fd3c4e0e3cc47cba4522d46
USER <USER@DESKTOP-JRUJ6S0>
2026-05-17 19:03:22 +0300

    dd

--- StudentBinding/MainPage.xaml.cs
+++ StudentBinding/MainPage.xaml.cs
@@ -30,22 +30,22 @@ namespace StudentBinding
             StudentNumber0.ProfilePicture = "dotnet_bot.png";
             StudentNumber0.FullName = "ישראל ישראלי";
             StudentNumber0.Birthday = new DateTime(2000, 1, 1);
-            StudentNumber0.Age = 16;
+            StudentNumber0.Age = 26;
             StudentNumber0.EmailAddress = "joe@gmail.com";
             StudentNumber0.PhoneNumber = "050-1234567";
 
-            StudentNumber1.ProfilePicture = "BobTheMinion.png";
-            StudentNumber1.FullName = "Bob The Minion";
-            StudentNumber1.Birthday = new DateTime(2010, 7, 9);
-            StudentNumber1.Age = 12;
-            StudentNumber1.EmailAddress = "BobTheMinion@Gmail.com";
+            StudentNumber1.ProfilePicture = "daniel.png";
+            StudentNumber1.FullName = "daniel";
+            StudentNumber1.Birthday = new DateTime(2006, 7, 9);
+            StudentNumber1.Age = 20;
+            StudentNumber1.EmailAddress = "daniel@Gmail.com";
             StudentNumber1.PhoneNumber = "123-456-7890";
 
-            StudentNumber2.ProfilePicture = "AgnesTheBest.png";
-            StudentNumber2.FullName = "Agnes The Best";
-            StudentNumber2.Birthday = new DateTime(2012, 3, 15);
-            StudentNumber2.Age = 15;
-            StudentNumber2.EmailAddress = "AgnesTheBest@Gmail.com";
+            StudentNumber2.ProfilePicture = "noa.png";
+            StudentNumber2.FullName = "noa";
+            StudentNumber2.Birthday = new DateTime(2004, 3, 15); 
+            StudentNumber2.Age = 22;
+            StudentNumber2.EmailAddress = "noa@Gmail.com";
             StudentNumber2.PhoneNumber = "098-765-4321";
 
             DisplayedStudent = StudentNumber0;
